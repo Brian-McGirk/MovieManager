0e1b8f31c5c814fc19344bba148c598fc302f2d2
Brian McGirk <bmcgirk2014@gmail.com>
2018-06-26 16:00:13 -0500

    Add Movies coming soon section to  main page along with movie titles, images and descriptions

--- MovieManager/Controllers/UserController.cs
+++ MovieManager/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using MovieManager.Data;
 using MovieManager.Models;
@@ -22,6 +23,54 @@ namespace MovieManager.Controllers
 
         public IActionResult Index()
         {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://movieweb.com/movies/coming-soon/");
+
+            // Gets names
+            //HtmlNode[] node = doc.DocumentNode.SelectNodes("//div[@class='lister-item-content']/h3/a").ToArray();
+
+            //List<string> test = new List<string>();
+
+            //Gets link text
+            //foreach(var n in node)
+            //{
+            //   test.Add(n.Attributes["href"].Value);
+            //}
+
+            //Get the first 4 image tags
+            HtmlNode[] imgs = doc.DocumentNode.SelectNodes("//div[@class='new-movies-items']/section[@class='movie'][position()<5]/figure/a/img").ToArray();
+
+            List<string> href = new List<string>();
+            List<string> title = new List<string>();
+            List<string> description = new List<string>();
+
+            //Extract the link to the image and put it in a list
+            foreach (var img in imgs)
+            {
+                href.Add(img.Attributes["src"].Value);
+            }
+
+            //Get the first 4 tags with titles
+            HtmlNode[] titles = doc.DocumentNode.SelectNodes("//div[@class='new-movies-items']/section[@class='movie'][position()<5]/div[@class='movie-description']/h2/a").ToArray();
+
+            //Extract the title names and add it to a list
+            foreach (var t in titles)
+            {
+                title.Add(t.InnerText);
+            }
+
+
+            HtmlNode[] descriptions = doc.DocumentNode.SelectNodes("//div[@class='new-movies-items']/section[@class='movie'][position()<5]/div[@class='movie-description']/div[@class='movie-synopsis']/p").ToArray();
+
+            foreach (var d in descriptions)
+            {
+                description.Add(d.InnerText);
+            }
+
+            ViewBag.href = href;
+            ViewBag.title = title;
+            ViewBag.description = description;
+
             return View();
         }
 
